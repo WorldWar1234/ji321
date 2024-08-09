88040a25e58f35c059bfa68978d28e38d64aeb91
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-08-09 17:38:40 +0530

    Update redirect.js

--- src/redirect.js
+++ src/redirect.js
@@ -1,10 +1,12 @@
-function redirect(params, res) {
+function redirect(req, res) {
+  const { url } = req.params;
+
   res.setHeader('content-length', 0);
   res.removeHeader('cache-control');
   res.removeHeader('expires');
   res.removeHeader('date');
   res.removeHeader('etag');
-  res.setHeader('location', encodeURI(params.url));
+  res.setHeader('location', encodeURI(url));
   res.setHeader('content-encoding', 'identity');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
